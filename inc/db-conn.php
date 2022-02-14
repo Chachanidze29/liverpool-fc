98e6648f305f73d3b0c88e98c0e1caabdfc1c641
Avtandil Chachanidze <83176152+Chachanidze29@users.noreply.github.com>
2022-02-14 18:18:42 +0400

    connecting to db

--- inc/db-conn.php
+++ inc/db-conn.php
@@ -1,9 +1,12 @@
 <?php
 
-$dbName = 'abcd0001';
-$dbUser = 'root';
-$dbPass = '';
-$dbHost = 'localhost';
+$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
+
+$dbName = substr($url['path'], 1);
+$dbUser = $url['user'];
+$dbPass = $url['pass'];
+$dbHost = $url['host'];
 
 try {
     $conn = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName, $dbUser, $dbPass);
